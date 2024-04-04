3477e104bef1e01bf85d9bde0a5630270f105630
lcordoval <lcordova@multiplied.com>
2024-04-03 21:08:47 -0500

    clase 03 mvc controller, views, partialviews y conexion entre controler y vistas

---
+++ Clase03/SolMVClase03/AppMVC/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using AppMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            var model = new StudentsViewModel();
+            return View(model);
+        }
+
+    }
+}
---
+++ Clase03/SolMVClase03/AppMVC/Models/StudentsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Principal;
+
+namespace AppMVC.Models
+{
+    public class StudentsViewModel
+    {
+        public StudentViewModel StudentToSave { get; set; }
+        public List<StudentViewModel> StudentList { get; set; }
+        public StudentsViewModel()
+        {
+            StudentList = new List<StudentViewModel>();
+            StudentList.Add(new StudentViewModel() { Name = "Jesus Alzamora", Age = 33, Career = "Periodismo" });
+            StudentList.Add(new StudentViewModel() { Name = "Fiorella Garcia", Age = 30, Career = "Educación" });
+        }
+    }
+
+    public class StudentViewModel {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Career { get; set; }
+    }
+
+}
