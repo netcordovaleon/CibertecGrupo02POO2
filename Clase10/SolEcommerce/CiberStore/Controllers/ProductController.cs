522d62e7fc70664593932767f10411e73ef8efd6
lcordoval <lcordova@multiplied.com>
2024-05-05 09:18:02 -0500

    add script for BDStore

---
+++ Clase10/SolEcommerce/CiberStore/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CiberStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ scriptCiberStore/DataOfProducts_Example.sql
@@ -0,0 +1,90 @@
+USE [CiberStoreDB]
+GO
+
+INSERT INTO [dbo].[product]
+           ([Name]
+           ,[Stock]
+           ,[Price]
+           ,[Image])
+     VALUES
+           ('Laptop Gamer'
+           ,20
+           ,3000
+           ,'https://dcmallio.vtexassets.com/arquivos/ids/327112716/UF06294.jpg')
+GO
+
+
+INSERT INTO [dbo].[product]
+           ([Name]
+           ,[Stock]
+           ,[Price]
+           ,[Image])
+     VALUES
+           ('Silla Gamer'
+           ,20
+           ,3000
+           ,'https://www.proshopper.pe/cdn/shop/products/SGAZ1_1_3a8a10fe-a1cd-4445-9634-95c51a8ed7e5_480x480.jpg')
+GO
+
+INSERT INTO [dbo].[product]
+           ([Name]
+           ,[Stock]
+           ,[Price]
+           ,[Image])
+     VALUES
+           ('Mouse Gamer'
+           ,130
+           ,45
+           ,'https://plazavea.vteximg.com.br/arquivos/ids/28315729-512-512/imageUrl_1.jpg')
+GO
+
+INSERT INTO [dbo].[product]
+           ([Name]
+           ,[Stock]
+           ,[Price]
+           ,[Image])
+     VALUES
+           ('Camara video llamadas'
+           ,34
+           ,35
+           ,'https://oechsle.vteximg.com.br/arquivos/ids/4507498/image-c8bc36823841432d9ddf14d3bdcf3306.jpg')
+GO
+
+INSERT INTO [dbo].[product]
+           ([Name]
+           ,[Stock]
+           ,[Price]
+           ,[Image])
+     VALUES
+           ('Microfono'
+           ,34
+           ,35
+           ,'https://realplaza.vtexassets.com/arquivos/ids/16667555/image-0f189443ef2646e9b18a425b44a17e47.jpg')
+GO
+
+INSERT INTO [dbo].[product]
+           ([Name]
+           ,[Stock]
+           ,[Price]
+           ,[Image])
+     VALUES
+           ('Mousepad'
+           ,65
+           ,11
+           ,'https://promart.vteximg.com.br/arquivos/ids/4897716-1000-1000/image-45c0309ea13848e2b32b4241e3149bca.jpg')
+GO
+
+
+INSERT INTO [dbo].[product]
+           ([Name]
+           ,[Stock]
+           ,[Price]
+           ,[Image])
+     VALUES
+           ('Escritorio'
+           ,800
+           ,3
+           ,'https://promart.vteximg.com.br/arquivos/ids/7042927-1000-1000/image-4d6c32c5acc949f7a5e471626ae51438.jpg')
+GO
+
+
